6b17c71bbe4dae790c5ca6c0adc34a3bf56b308f
Ryan <ryankane180@gmail.com>
2024-01-29 21:27:44 +0000

    Reading from DB

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 import { Auth } from "./components/auth";
+import { db } from './config/firebase';
+import { useEffect, useState } from 'react';
+import { getDocs, collection } from 'firebase/firestore';
 
 function App() {
+  const [bookList, setBookList] = useState([]);
+  const bookCollectionRef = collection(db, 'books');
+
+  useEffect(() => {
+    const getBookList = async () => {
+      try{
+        //read data
+        //set book state to data
+        const data = await getDocs(bookCollectionRef);
+        const filteredData = data.docs.map((doc) => ({...doc.data(), id: doc.id}));
+        console.log(filteredData);
+        setBookList(filteredData);
+      } catch(err) {
+        console.error(err);
+      }
+     
+    }
+
+    getBookList();
+  }, []);
+
+  
+
+
   return (
     <div className="App">
       <Auth />
+      <div>
+        {bookList.map((book) => (
+          <div>
+            <h1>{book.title}</h1>
+            <h2>{book.author}</h2>
+            <p>Year: {book.year}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
